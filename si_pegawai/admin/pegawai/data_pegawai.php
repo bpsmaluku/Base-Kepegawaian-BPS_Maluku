233c7f122b63cb694587088333336e771918335c
Harfel12 <85319667+harfely@users.noreply.github.com>
2023-02-27 19:18:27 +0700

    update

--- si_pegawai/admin/pegawai/data_pegawai.php
+++ si_pegawai/admin/pegawai/data_pegawai.php
@@ -40,7 +40,23 @@
                     <a href="?page=add-pegawai" class="btn btn-primary">
                         <i class="fa fa-edit"></i> Tambah Data</a>
                     <a target="_blank" href="export_excel.php" class="btn btn-primary"> EXPORT KE EXCEL</a>
-                    </a>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
+                            <div class="dataTables_length" id="example1_length"><label>Show <select
+                                        name="example1_length" aria-controls="example1"
+                                        class="custom-select custom-select-sm form-control form-control-sm">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select> entries</label></div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search"
+                                        class="form-control form-control-sm" placeholder=""
+                                        aria-controls="example1"></label></div>
+                        </div>
+                    </div>
                 </div>
                 <br>
                 <table id=" example1" class="table table-bordered table-striped">
--- si_pegawai/export_excel.php
+++ si_pegawai/export_excel.php
@@ -1,3 +1,10 @@
+<?php
+    if(isset($_GET['kode'])){
+        $sql_cek = "SELECT * FROM tb_pegawai WHERE nip='".$_GET['kode']."'";
+        $query_cek = mysqli_query($koneksi, $sql_cek);
+        $data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
+    }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -6,32 +13,6 @@
 </head>
 
 <body>
-    <style type="text/css">
-    body {
-        font-family: sans-serif;
-    }
-
-    table {
-        margin: 20px auto;
-        border-collapse: collapse;
-    }
-
-    table th,
-    table td {
-        border: 1px solid #3c3c3c;
-        padding: 3px 8px;
-
-    }
-
-    a {
-        background: blue;
-        color: #fff;
-        padding: 8px 10px;
-        text-decoration: none;
-        border-radius: 2px;
-    }
-    </style>
-
     <?php
 	header("Content-type: application/vnd-ms-excel");
 	header("Content-Disposition: attachment; filename=Data Pegawai.xls");
@@ -44,70 +25,15 @@
     <table border="1">
         <tr>
             <th>No</th>
-            <th>Nama Pegawai</th>
-            <th>Alamat</th>
-            <th>No.Telp</th>
+            <th>NIP</th>
+            <th>Nama</th>
+            <th>Status</th>
+            <th>Dosir</th>
+            <th>Rak</th>
         </tr>
         <tr>
-            <td>1</td>
-            <td>Sulaiman</td>
-            <td>Jakarta</td>
-            <td>0829121223</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Diki Alfarabi Hadi</td>
-            <td>Jakarta</td>
-            <td>08291212211</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Zakaria</td>
-            <td>Medan</td>
-            <td>0829121223</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Alvinur</td>
-            <td>Jakarta</td>
-            <td>02133324344</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Muhammad Rizani</td>
-            <td>Jakarta</td>
-            <td>08231111223</td>
-        </tr>
-        <tr>
-            <td>6</td>
-            <td>Rizaldi Waloni</td>
-            <td>Jakarta</td>
-            <td>027373733</td>
-        </tr>
-        <tr>
-            <td>7</td>
-            <td>Ferdian</td>
-            <td>Jakarta</td>
-            <td>0829121223</td>
-        </tr>
-        <tr>
-            <td>8</td>
-            <td>Fatimah</td>
-            <td>Jakarta</td>
-            <td>23432423423</td>
-        </tr>
-        <tr>
-            <td>9</td>
-            <td>Aminah</td>
-            <td>Jakarta</td>
-            <td>0829234233</td>
-        </tr>
-        <tr>
-            <td>10</td>
-            <td>Jafarudin</td>
-            <td>Jakarta</td>
-            <td>0829239323</td>
-        </tr>
+
+
     </table>
 </body>
 
